29b72045c00f0c696e336b763f7c6e7353634f45
madalv <magalvlada.s@gmail.com>
2022-09-13 20:18:32 +0300

    multiple coroutines that cook orders

--- src/main/kotlin/com/madalv/Order.kt
+++ src/main/kotlin/com/madalv/Order.kt
@@ -1,13 +1,12 @@
 package com.madalv
 
-
 import kotlinx.serialization.SerialName
 
 @kotlinx.serialization.Serializable
-data class Order(val id: Int,
-                 val items: List<Int>,
-                 val priority: Int,
-                 @SerialName("max_wait")
-                 val maxWait: Double,
-                 @SerialName("pick_up_time")
-                 val pickupTime: Long)
+data class Order( @SerialName("order_id") val id: Int,
+                  @SerialName("table_id") val tableId: Int,
+                  @SerialName("waiter_id") val waiterId: Int,
+                  val items: List<Int>,
+                  val priority: Int,
+                  @SerialName("max_wait") val maxWait: Double,
+                  @SerialName("pick_up_time") val pickupTime: Long)
--- src/main/kotlin/com/madalv/plugins/Routing.kt
+++ src/main/kotlin/com/madalv/plugins/Routing.kt
@@ -1,11 +1,16 @@
 package com.madalv.plugins
 
+import com.madalv.Cook
 import com.madalv.Order
+import com.madalv.cfg
+import com.madalv.logger
 import io.ktor.server.routing.*
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.request.*
+import kotlinx.coroutines.launch
+import java.util.concurrent.ThreadLocalRandom
 
 fun Application.configureRouting() {
 
@@ -14,8 +19,17 @@ fun Application.configureRouting() {
             call.respondText("Hello Kitchen!")
         }
         post("/order") {
-            val order: Order = call.receive()
-            println(order)
+            launch {
+                val r = ThreadLocalRandom.current()
+                val order: Order = call.receive()
+                val cook = Cook(1,
+                    1,
+                    "Anders Erickson",
+                    "Do you know how to make 5 margaritas?",
+                    r.nextInt(1, cfg.nrCooks + 1))
+
+                cook.executeOrder(order)
+            }
         }
     }
 }
